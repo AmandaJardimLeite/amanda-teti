8902284588415036ce8b599e381baa6147061228
augustoppk <yzukke@gmail.com>
2025-08-25 10:57:46 -0400

    add initial project

---
+++ src/modules/scenario/dto/create-scenario.dto.ts
@@ -0,0 +1,5 @@
+export class CreateScenarioDto {
+    tittle: string;
+    description: string;
+    //options: Option[];
+}
---
+++ src/modules/scenario/repository/create-scenario.repository.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@nestjs/common";
+import { CreateScenarioDto } from "../dto/create-scenario.dto";
+import { Scenario } from "../entities/scenario.entity";
+import { PrismaService } from "src/shared/database/prisma.database";
+
+@Injectable()
+export class CreateScenarioRepository{
+    constructor(private readonly prisma: PrismaService) {}
+
+    async create(data: CreateScenarioDto) {
+        const scenario = await this.prisma.scenario.create({
+            data,
+        });
+        return Scenario;
+    }
+    
+
+}
