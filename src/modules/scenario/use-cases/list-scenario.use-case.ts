625fbc1aaf723ad5da9058fb3b78b42413d7492d
AmandaJardimLeite <amandasantosjardim05@gmail.com>
2025-09-15 11:09:12 -0400

    sla

--- src/modules/scenario/use-cases/list-scenario.use-case.ts
+++ src/modules/scenario/use-cases/list-scenario.use-case.ts
@@ -1,2 +1,20 @@
-export * from './create-scenario.use-case';
-export * from './list-scenario.use-case';
+import { Injectable, Logger } from "@nestjs/common";
+import { ListScenarioRepository} from "../repository/list-scenario.repository";
+
+@Injectable()
+export class ListScenarioUseCase {
+    constructor(
+        private readonly listScenarioRepository: ListScenarioRepository,
+        private readonly logger: Logger,
+    ) { }
+
+    async execute() {
+        try {
+            const scenario = await this.listScenarioRepository.list();
+            return scenario;
+        } catch (error) {
+            this.logger.error(error);
+            throw error;
+        }
+    }
+}
